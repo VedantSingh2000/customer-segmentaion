128e4cb4a7e7e153e8bb986d222c2a2e384f3870
VedantSingh2000 <97618130+VedantSingh2000@users.noreply.github.com>
2025-04-14 13:58:14 +0530

    Add files via upload

---
+++ app-one.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "0d2a68f1-571e-487a-896e-6cb4c9f8257c",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
